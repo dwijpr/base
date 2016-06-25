470c06eb944459d6c2ef1f9a625cb7f3793bc392
Dwi Prabowo <Dwi Prabowo>
2016-06-25 09:40:44 +0700

    update config timezone

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,10 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">
+                    Laravel 5
+                    {{ date('Y-m-d h:i:s') }}
+                </div>
             </div>
         </div>
     </body>
