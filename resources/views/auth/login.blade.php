8f747f1123dd0ef9580810b73618e424dd6dc78b
dwijpr <dwijpr@gmail.com>
2016-06-25 19:10:56 +0700

    optimize login, register page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,32 +7,37 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Login</div>
                 <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
-                        {{ csrf_field() }}
-
+                    {{ Form::open([
+                        'url' => 'login',
+                        'class' => 'form-horizontal',
+                        'role' => 'form',
+                    ]) }}
                         {{ Form::bsText('email') }}
                         {{ Form::bsPassword('password') }}
-
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <div class="checkbox">
                                     <label>
-                                        <input type="checkbox" name="remember"> Remember Me
+                                        <input
+                                            type="checkbox"
+                                            name="remember"
+                                        > Remember Me
                                     </label>
                                 </div>
                             </div>
                         </div>
-
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
                                     <i class="fa fa-btn fa-sign-in"></i> Login
                                 </button>
-
-                                <a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
+                                <a
+                                    class="btn btn-link"
+                                    href="{{ url('/password/reset') }}"
+                                >Forgot Your Password?</a>
                             </div>
                         </div>
-                    </form>
+                    {{ Form::close() }}
                 </div>
             </div>
         </div>
