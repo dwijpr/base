53f7a69333ae2210bbadd3b5f97e2b141e49ecd1
dwijpr <dwijpr@gmail.com>
2016-07-04 05:57:22 +0700

    add+update migrations

--- database/migrations/2016_07_04_055454_create_socials_table.php
+++ database/migrations/2016_07_04_055454_create_socials_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateNumbersTable extends Migration
+class CreateSocialsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,10 +12,10 @@ class CreateNumbersTable extends Migration
      */
     public function up()
     {
-        Schema::create('numbers', function (Blueprint $table) {
+        Schema::create('socials', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->unsigned()->index();
-            $table->string('label');
+            $table->string('key');
             $table->string('value');
             $table->timestamps();
         });
@@ -28,6 +28,6 @@ class CreateNumbersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('numbers');
+        Schema::drop('socials');
     }
 }
