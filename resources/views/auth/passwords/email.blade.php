712a5671cad5d5f1ddd3b1cbb0aa54d34f153ab3
dwijpr <dwijpr@gmail.com>
2016-07-06 16:35:01 +0700

    update forms

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -14,31 +14,21 @@
                     </div>
                 @endif
 
-                <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
-                    {{ csrf_field() }}
-
-                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                        <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                        <div class="col-md-6">
-                            <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
-
-                            @if ($errors->has('email'))
-                                <span class="help-block">
-                                    <strong>{{ $errors->first('email') }}</strong>
-                                </span>
-                            @endif
-                        </div>
-                    </div>
-
+                {{ Form::open([
+                    'url' => '/password/email',
+                    'class' => 'form-horizontal',
+                    'role' => 'form',
+                ]) }}
+                    {{ Form::bsText('email') }}
                     <div class="form-group">
-                        <div class="col-md-6 col-md-offset-4">
+                        <div class="col-sm-6 col-sm-offset-4">
                             <button type="submit" class="btn btn-primary">
-                                <i class="fa fa-btn fa-envelope"></i> Send Password Reset Link
+                                <i class="fa fa-btn fa-envelope"></i>
+                                Send Password Reset Link
                             </button>
                         </div>
                     </div>
-                </form>
+                {{ Form::close() }}
             </div>
         </div>
     </div>
