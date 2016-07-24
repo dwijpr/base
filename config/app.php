59c2462b3f49d2617e126e262308d05f563531d7
dwijpr <dwijpr@gmail.com>
2016-07-24 08:09:46 +0700

    load Intervention Image

--- config/app.php
+++ config/app.php
@@ -179,6 +179,7 @@ return [
          */
         Collective\Html\HtmlServiceProvider::class,
         Orangehill\Iseed\IseedServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,
         GrahamCampbell\Markdown\MarkdownServiceProvider::class,
     ],
 
@@ -231,6 +232,7 @@ return [
          */
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Image' => Intervention\Image\Facades\Image::class,
         'Markdown' => GrahamCampbell\Markdown\Facades\Markdown::class,
 
     ],
